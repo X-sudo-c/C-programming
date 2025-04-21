ad08da77473d28b49f70c01aeea04fcc4ad6b799
reishi <x1dragonn@gmail.com>
2025-04-21 20:51:32 +0000

    how to comment in C

---
+++ comments.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    /* escape sequence = character combination consisting of a backslash \
+                          followed by a letter or combination of digits.
+                          They specify actions within a line or string of text.
+                          \n = new line 
+                          \t = tab
+
+    
+    */
+
+
+    printf(" i\n like\n pizza\n");//this puts the code on 3 different lines du
+
+    printf("\"I like pizza s\"");
+
+
+
+}
