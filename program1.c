7e85f2f765aa14b92115b3ce9d5ab8aaac0c3257
reishi <x1dragonn@gmail.com>
2025-04-22 13:53:05 +0000

    programming exercise

---
+++ program1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+
+int main(){
+    char first_name[22] = "kira";
+    char last_name[22]= "rei";
+
+
+    printf("%s %s \n", first_name, last_name);
+    printf("%s \n", first_name);
+    printf("%s \n", last_name);
+    printf("%s %s \n", first_name, last_name);
+    
+    return 0;
+}
