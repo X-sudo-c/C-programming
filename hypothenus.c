af1e7f14bb053f5d974f01f5be6bfa02863a7143
reishi <x1dragonn@gmail.com>
2025-04-24 02:27:09 +0000

    simple hypothenus program

---
+++ hypothenus.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdbool.h>
+
+void can(void);
+
+int main(){
+    double a;
+    double b;
+    double c;
+
+    printf("what is A \n" );
+    scanf("%lf", &a);
+
+
+    printf("What is B \n");
+    scanf("%lf", &b);
+
+
+    c = sqrt(pow(a,2)+pow(b,2));
+
+
+    printf("%lf", c);
+
+    return 0;
+
+
+
+
+
+
+
+
+
+
+}
+
+
