b6da4769b2f367edfae78f54d9e1a567940632dc
reishi <x1dragonn@gmail.com>
2025-04-29 20:55:34 +0000

    || OR  operater in C

---
+++ orlogical.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdio.h>
+
+
+
+int main(){
+
+    // logical operater (or) "||"
+    float temp = 31;
+
+
+
+    if ( temp <= 0 || temp >= 30 ){
+
+        printf("\nThe weather is bad");
+
+    }
+    else{
+        printf("\n The weather is good");
+    }
+
+//damn  we at OR logical Operater damn very soon ill be coding rootkits 
+    return 0;
+}
