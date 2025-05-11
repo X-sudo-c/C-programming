5890e523f226611ecdfb313aea4f1d0bda8864e0
reishi <x1dragonn@gmail.com>
2025-05-11 10:02:24 +0000

    using sizeoff() and also looping through a string using pointers

--- sizeoffoperator/loops.c
+++ sizeoffoperator/loops.c
@@ -6,13 +6,22 @@ int main(){
     double prices[] = { 5.0, 10.0, 15.0, 25.0, 20.0, 30.0} ;
 
 
-    printf("%d bytes\n", sizeof(prices)); // using sizeof() shows you  how big an Object is in bytes output = 48bytes
+    printf("%lu bytes\n", sizeof(prices)); // using sizeof() shows you  how big an Object is in bytes output = 48bytes
 
     for (int i = 0 ; i < sizeof(prices)/sizeof(prices[0]); i++)
     {
-        printf("$%.2lf\n", prices[i]);
+        printf("$ %.2lf\n", prices[i]);
+       
     }
 
+
+    const char *people[] ={"char", "john", "ethan", "leat", "Sarah", "Esther", "rita"};
+
+    for (int i = 0; i < sizeof(people)/sizeof(people[0]); i++){
+        printf("%s \n",people[i]);
+
+
+    }
 
 
 
