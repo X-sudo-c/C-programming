3706fab928381fe1211f57f47b2f0c1917ce1bb3
reishi <x1dragonn@gmail.com>
2025-05-12 22:08:34 +0000

    files

---
+++ swaping2varvar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+
+    char x = 'X';
+    char y = 'Y';
+
+
+    printf("Ill do this later");
+    printf("My KAli linux crashed i have to set up a new one");
+
+
+}
