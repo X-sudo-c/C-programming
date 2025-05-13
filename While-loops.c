cd2363ec1f5ea3e5f69129c5dcff1120f0420f37
Aheng jr O.A <91377224+X-sudo-c@users.noreply.github.com>
2025-05-13 23:57:55 +0000

    Update While-loops.c
    commit

--- While-loops.c
+++ While-loops.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <string.h>
 
-
+void func(){
+    int num = 34;
+    printf("i am commiting this so that i get a commit for today %d", num);
+    
+}
 
 int main(){
     char name [25];
     printf("WHat is your name:\n");
     fgets(name, 25, stdin);
     name[strlen(name)-1] = '\0';
-
+    
+    func();
+    
 
     while (strlen(name)==0)
     {
