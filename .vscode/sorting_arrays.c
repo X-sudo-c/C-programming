5166e5dd9472d589961de8533e029dd292dc9ac6
reishi <x1dragonn@gmail.com>
2025-05-24 20:30:23 +0000

    continue this

---
+++ .vscode/sorting_arrays.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <string.h>
+
+void sort(int array[],int size){
+    for(int i = 0 ; i < size - 1; i++)
+    {
+        for(int j = 0; i < size -1; )
+    }
+
+}
+
+
+
+int main(){
+    int array[] = {9, 1, 8, 2, 7, 3, 6, 4, 5};
+    int size = sizeof(array)/sizeof(array[0]);
+
+
+    return 0;
+}
