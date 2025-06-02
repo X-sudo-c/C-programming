fd2ae5539cff9cd4e0ab322fcc12b465a23e7f9c
X-sudo-c <x1dragonn@gmail.com>
2025-06-02 13:07:25 +0000

    ~

---
+++ arrayofstructs.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+struct Student{
+    char name[12];
+    float gpa ;
+};
+
+
+int main(){
+
+    struct Student student1 = {"Spongebob", 3.0};
+    struct Student student2 = {"Patrick", 2.0};
+    struct Student student3 = {"Sandy", 4.0};
+    struct Student student4= {"Squidward", 2.0};
+
+    // in the above code we assigned values to our structs 
+
+    struct Student students[] = {student1, student2, student3, student4}; //we create another struct that stores the assigned structs in an array
+
+    for (int i = 0; i < sizeof(students)/sizeof(students[0]); i++)
+    {
+        printf("-12%s\n", students[i].name);
+        printf("%.2f\n", students[i].gpa);
+
+    }
+
+
+
+
+
+
+    return 0;
+}
