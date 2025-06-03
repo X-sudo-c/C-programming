1744c3a7a8deb2441b7c2f7166eabb5e299c0305
X-sudo-c <x1dragonn@gmail.com>
2025-06-03 23:48:29 +0000

    enums

--- enums.c
+++ enums.c
@@ -13,7 +13,36 @@ int main(){
 
     printf("%d", today); //enums are not strings but the can be treated as integers 
 
+
+
+    // now how does enum make code more readable
+
+    // if(today == 1 || today == 7)
+    // {
+    //     printf("\nIts the weekend party time!");
+    // }
+    // else{
+    //     printf("I have to work today :");
+    // }
+
+    // the above code doesnt use enums
+
+    
+    if(today == Sun|| today == Sat)
+    {
+        printf("\nIts the weekend party time!");
+    }
+    else{
+        printf("I have to work today :");
+    }
+
+    // the code above utilizes the feature enum has in the C language 
+    // makes sense but doesnt make enough sense to be a solid use case
+    //future kira comment when you see this hopefully you are waayy better at C
+
     return 0;
 }
+
+
 
 //personally i dont see the use case of a feature like enums maybe as time goes on i will learn it 
